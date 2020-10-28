babd67feebb8061023f4351a3785ca0325fe642a
olegpetrov93 <sanderos2008@yandex.ru>
2020-10-28 23:41:25 +0300

    addicting research

--- MyPlaces/TableViewController.swift
+++ MyPlaces/TableViewController.swift
@@ -12,8 +12,19 @@ import RealmSwift
 
 class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var places : Results<Place>!
-    var ascendingSorted = true
+    private var places : Results<Place>!
+    private var ascendingSorted = true
+    
+    private var filtredPlaces: Results<Place>!
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var searchBarIsEmty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmty
+    }
+    
 
     @IBOutlet weak var reverstSortingButton: UIBarButtonItem!
     @IBOutlet weak var segmentetControl: UISegmentedControl!
@@ -26,6 +37,14 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         segmentetControl.setTitle("Date", forSegmentAt: 1)
         
         places = realm.objects(Place.self)
+        
+        //Setup the search controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
 
     }
 
@@ -34,6 +53,9 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        if isFiltering {
+        return filtredPlaces.count
+        }
         return places.isEmpty ? 0 : places.count
     }
 
@@ -41,7 +63,13 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CutomViewCellTableViewCell
 
-        let place = places[indexPath.row]
+        var place = Place()
+        
+        if isFiltering {
+            place = filtredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
 
         cell.nameLabel.text = place.name
         cell.locationLabel.text = place.location
@@ -76,8 +104,15 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
+            
+            
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
-            let place = places[indexPath.row]
+            var place = Place()
+            if isFiltering {
+                place = filtredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
             let newPlaceVC = segue.destination as! NewPlaceViewController
             newPlaceVC.currentPlace = place
         }
@@ -117,5 +152,20 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         }
         tableView.reloadData()
     }
+    
+}
+extension TableViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearChText(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearChText(_ searchText: String) {
+        
+        filtredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText, searchText)
+        
+        tableView.reloadData()
+    }
+    
     
 }
